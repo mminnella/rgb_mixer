b0f982cca739080aa0ab0466c160d2c5b9ca2302
Massimiliano Minnella <massimiliano.minnella@gmail.com>
2021-04-12 12:13:47 +0200

    debounced done

--- src/debounce.v
+++ src/debounce.v
@@ -6,5 +6,25 @@ module debounce (
     input wire button,
     output reg debounced
 );
+	reg [7:0] b_history;
+	
+	always @(posedge clk) begin	
+	
+		b_history <= { b_history[6:0], button };
+		if ( b_history == 8'b1111_1111) begin
+			debounced <= 1;
+		end
+
+		if (b_history == 8'b0000_000) begin
+			debounced <= 0;
+		end
+
+		if (reset) begin
+			b_history <= 0;
+			debounced <= 0;
+		end
+	
+	
+	end
 
 endmodule
--- src/pwm.v
+++ src/pwm.v
@@ -1,21 +1,26 @@
 `default_nettype none
 `timescale 1ns/1ns
-module pwm (
-    input wire clk,
-    input wire reset,
-    output wire out,
-    input wire [7:0] level
-    );
+module pwm #(
+	parameter WIDTH = 8,
+ 	parameter INVERT = 0
+	)(
+	input wire clk,
+	input wire reset,
+	output wire out,
+	input wire [WIDTH-1:0] level
+	);
 
-    reg [7:0] counter;
+	reg [WIDTH-1:0] counter;
+	wire pwm_on = counter < level;
 
-    always @(posedge clk) begin
-	    counter <= counter + 1'b1;
+	always @(posedge clk) begin
+		counter <= counter + 1'b1;
+		if (reset) begin
+			counter <= 0;
+		end
+	end
 
-	    if (reset) begin
-		    counter <= 0;
-	    end
-    end
-    assign out = (reset == 1'b1) ? 1'b0:
-	    (counter < level) ? 1'b1 : 1'b0; 
+	assign out = (reset == 1'b1) ? 1'b0:
+	       (INVERT == 1'b0) ? pwm_on: ! pwm_on;
+
 endmodule
