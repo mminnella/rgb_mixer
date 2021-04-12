ec386b0331ee5c2c4321f55286db14eacfd9463f
Massimiliano Minnella <massimiliano.minnella@gmail.com>
2021-04-12 19:59:01 +0200

    added 12MHz clock divider,but make test_rgb_mixer fails

--- src/rgb_mixer.v
+++ src/rgb_mixer.v
@@ -17,25 +17,36 @@ module rgb_mixer (
     wire enc1_a_db, enc1_b_db;
     wire enc2_a_db, enc2_b_db;
     wire [7:0] enc0, enc1, enc2;
+    reg [7:0] clk_div;
+    wire slow_clk;
 
     // debouncers, 2 for each encoder
-    debounce #(.HIST_LEN(8)) debounce0_a(.clk(clk), .reset(reset), .button(enc0_a), .debounced(enc0_a_db));
-    debounce #(.HIST_LEN(8)) debounce0_b(.clk(clk), .reset(reset), .button(enc0_b), .debounced(enc0_b_db));
+    debounce #(.HIST_LEN(8)) debounce0_a(.clk(slow_clk), .reset(reset), .button(enc0_a), .debounced(enc0_a_db));
+    debounce #(.HIST_LEN(8)) debounce0_b(.clk(slow_clk), .reset(reset), .button(enc0_b), .debounced(enc0_b_db));
 
-    debounce #(.HIST_LEN(8)) debounce1_a(.clk(clk), .reset(reset), .button(enc1_a), .debounced(enc1_a_db));
-    debounce #(.HIST_LEN(8)) debounce1_b(.clk(clk), .reset(reset), .button(enc1_b), .debounced(enc1_b_db));
+    debounce #(.HIST_LEN(8)) debounce1_a(.clk(slow_clk), .reset(reset), .button(enc1_a), .debounced(enc1_a_db));
+    debounce #(.HIST_LEN(8)) debounce1_b(.clk(slow_clk), .reset(reset), .button(enc1_b), .debounced(enc1_b_db));
 
-    debounce #(.HIST_LEN(8)) debounce2_a(.clk(clk), .reset(reset), .button(enc2_a), .debounced(enc2_a_db));
-    debounce #(.HIST_LEN(8)) debounce2_b(.clk(clk), .reset(reset), .button(enc2_b), .debounced(enc2_b_db));
+    debounce #(.HIST_LEN(8)) debounce2_a(.clk(slow_clk), .reset(reset), .button(enc2_a), .debounced(enc2_a_db));
+    debounce #(.HIST_LEN(8)) debounce2_b(.clk(slow_clk), .reset(reset), .button(enc2_b), .debounced(enc2_b_db));
 
     // encoders
-    encoder #(.WIDTH(8)) encoder0(.clk(clk), .reset(reset), .a(enc0_a_db), .b(enc0_b_db), .value(enc0));
-    encoder #(.WIDTH(8)) encoder1(.clk(clk), .reset(reset), .a(enc1_a_db), .b(enc1_b_db), .value(enc1));
-    encoder #(.WIDTH(8)) encoder2(.clk(clk), .reset(reset), .a(enc2_a_db), .b(enc2_b_db), .value(enc2));
+    encoder #(.WIDTH(8)) encoder0(.clk(slow_clk), .reset(reset), .a(enc0_a_db), .b(enc0_b_db), .value(enc0));
+    encoder #(.WIDTH(8)) encoder1(.clk(slow_clk), .reset(reset), .a(enc1_a_db), .b(enc1_b_db), .value(enc1));
+    encoder #(.WIDTH(8)) encoder2(.clk(slow_clk), .reset(reset), .a(enc2_a_db), .b(enc2_b_db), .value(enc2));
 
     // pwm modules
-    pwm #(.WIDTH(8)) pwm0(.clk(clk), .reset(reset), .out(pwm0_out), .level(enc0));
-    pwm #(.WIDTH(8)) pwm1(.clk(clk), .reset(reset), .out(pwm1_out), .level(enc1));
-    pwm #(.WIDTH(8)) pwm2(.clk(clk), .reset(reset), .out(pwm2_out), .level(enc2));
+    pwm #(.WIDTH(8)) pwm0(.clk(slow_clk), .reset(reset), .out(pwm0_out), .level(enc0));
+    pwm #(.WIDTH(8)) pwm1(.clk(slow_clk), .reset(reset), .out(pwm1_out), .level(enc1));
+    pwm #(.WIDTH(8)) pwm2(.clk(slow_clk), .reset(reset), .out(pwm2_out), .level(enc2));
 
+    always @ (posedge clk) begin
+    	clk_div <= clk_div + 1'b1;
+	if (reset) begin
+		clk_div <=0;
+	end
+
+    end
+
+    assign slow_clk = (clk_div[7]) ? 1'b1 : 1'b0;
 endmodule
