da4f1fd9dad490c02a3168a1e9e0dd1e22889c1d
Massimiliano Minnella <massimiliano.minnella@gmail.com>
2021-04-16 23:38:58 +0200

    fixed pcf w/new names, works fine on icebreaker

--- src/rgb_mixer.v
+++ src/rgb_mixer.v
@@ -9,7 +9,7 @@ module rgb_mixer (
 );
     wire [2:0] enca_db; //enca_db0, enca_db1, enca_db2
     wire [2:0] encb_db; //encb_db0, encb_db1, encb_db3
-    wire [7:0] enc0, enc1, enc2;
+    wire [23:0] enc; //enc0 [23:16], enc1[15:8], enc2[7:0]; [7+i:i] con i da 0 a 16
     reg [7:0] clk_div;
     wire clk;
 
@@ -24,14 +24,14 @@ module rgb_mixer (
     debounce #(.HIST_LEN(8)) debounce2_b(.clk(clk), .reset(reset), .button(encb[2]), .debounced(encb_db[2]));
 
     // encoders
-    encoder #(.WIDTH(8)) encoder0(.clk(clk), .reset(reset), .a(enca_db[0]), .b(encb_db[0]), .value(enc0));
-    encoder #(.WIDTH(8)) encoder1(.clk(clk), .reset(reset), .a(enca_db[1]), .b(encb_db[1]), .value(enc1));
-    encoder #(.WIDTH(8)) encoder2(.clk(clk), .reset(reset), .a(enca_db[2]), .b(encb_db[2]), .value(enc2));
+    encoder #(.WIDTH(8)) encoder0(.clk(clk), .reset(reset), .a(enca_db[0]), .b(encb_db[0]), .value(enc[8*0 +: 8]));
+    encoder #(.WIDTH(8)) encoder1(.clk(clk), .reset(reset), .a(enca_db[1]), .b(encb_db[1]), .value(enc[8*1 +: 8]));
+    encoder #(.WIDTH(8)) encoder2(.clk(clk), .reset(reset), .a(enca_db[2]), .b(encb_db[2]), .value(enc[8*2 +: 8]));
 
     // pwm modules
-    pwm #(.WIDTH(8)) pwm0(.clk(clk), .reset(reset), .out(pwm_out[0]), .level(enc0));
-    pwm #(.WIDTH(8)) pwm1(.clk(clk), .reset(reset), .out(pwm_out[1]), .level(enc1));
-    pwm #(.WIDTH(8)) pwm2(.clk(clk), .reset(reset), .out(pwm_out[2]), .level(enc2));
+    pwm #(.WIDTH(8)) pwm0(.clk(clk), .reset(reset), .out(pwm_out[0]), .level(enc[8*0 +: 8]));
+    pwm #(.WIDTH(8)) pwm1(.clk(clk), .reset(reset), .out(pwm_out[1]), .level(enc[8*1 +: 8]));
+    pwm #(.WIDTH(8)) pwm2(.clk(clk), .reset(reset), .out(pwm_out[2]), .level(enc[8*2 +: 8]));
 
     always @ (posedge clk12MHz) begin
     	clk_div <= clk_div + 1'b1;
